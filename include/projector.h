2091448a93097c19f6239265e0375a3c9bd7aaa8
Jeffrey Liu <jeffliu604@gmail.com>
2023-04-30 21:39:07 -0500

    Start projection class

---
+++ include/projector.h
@@ -0,0 +1,25 @@
+#ifndef __PROJECTOR_H__
+#define __PROJECTOR_H__
+
+#include <string>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+#define OUT_SIZE 96;
+#define HEAT_SIZE 18;
+
+#define SUBJECT_NUM 9
+#define GESTURE_NUM 17
+#define JOINT_NUM 21
+#define MAX_IMAGE_NUM 500
+#define MAX_DEPTH 2001
+
+class Projector
+{
+
+};
+
+#endif
