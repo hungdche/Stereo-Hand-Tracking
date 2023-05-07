48c008de4d7e784546ff3a820fc8a4d3685e162e
Henry Che <daihung.che@gmail.com>
2023-05-07 15:44:29 -0500

    Fix Torch installation

--- include/hand_pose_estimation/heatmap_fusion.h
+++ include/hand_pose_estimation/heatmap_fusion.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <eigen3/Eigen/Dense>
 #include <opencv2/opencv.hpp>
+#include <torch/torch.h>
+#include <torch/script.h>
 
 class HeatmapFuser {
 private:
@@ -13,7 +15,7 @@ private:
     double m_focal_length;
     int m_out_size, m_heat_size;
 
-    // 
+    
     std::array<double, 3> bbox_x;
     std::array<double, 3> bbox_y;
     std::array<double, 3> bbox_w;
@@ -21,8 +23,13 @@ private:
 
     // std::vector<std::vectoor
     
+public:
+    HeatmapFuser(int x_res, int y_res, double focal_length, int out_size, int m_heat_size);
 
-}
+    bool load_model(const std::string &path);
+    torch::Tensor load_tensor(const std::string &path);
+    
+};
 
 
 #endif // __HEATMAP_FUSION__
